93e7ce890c8b39a0d7d6421b6a74cc3bd1f74084
mo_kejp <mo_kejp@yahoo.co.jp>
2016-04-11 11:18:13 +0900

    contentTypeごとに関数を分けてみる

--- linebotapi/linebotapi.go
+++ linebotapi/linebotapi.go
@@ -4,6 +4,7 @@ import (
     "io"
     "bytes"
     "errors"
+    "strconv"
     "net/http"
     "encoding/json"
 )
@@ -22,7 +23,7 @@ const (
 )
 
 type Credential struct {
-    ChannelId string
+    ChannelId int
     ChannelSecret string
     ChannelMid string
 }
@@ -41,20 +42,43 @@ type Message struct {
     Content *MessageContent `json:"content,omitempty"`
 }
 type MessageContent struct {
-    Location *MessageContentLocation `json:"location,omitempty"`
     Id string `json:"id,omitempty"`
     ContentType uint8 `json:"contentType,omitempty"`
     From string `json:"from,omitempty"`
     CreatedTime int `json:"createdTime,omitempty"`
     To []string `json:"to,omitempty"`
     ToType int `json:"toType,omitempty"`
-    ContentMetadata map[string]string `json:"contentMetadata,omitempty"`
+    // Text
     Text string `json:"text,omitempty"`
+    // Image / Video / Audio
+    OriginalContentUrl string `json:"originalContentUrl,omitempty`
+    PreviewImageUrl string `json:"previewImageUrl,omitempty`
+    // Location
+    Location *MessageContentLocation `json:"location,omitempty"`
+    // Audio / Sticker
+    ContentMetadata map[string]string `json:"contentMetadata,omitempty"`
 }
 type MessageContentLocation struct {
     Title string `json:"title,omitempty"`
     Latitude float64 `json:"latitude,omitempty"`
     Longitude float64 `json:"longitude,omitempty"`
+}
+type MessageContentSticker struct {
+    Id string            // STKID
+    PackageId string     // STKPKGID
+    Version string       // STKVER
+}
+type MessageContentImage struct {
+    OriginalContentUrl string
+    PreviewImageUrl string
+}
+type MessageContentVideo struct {
+    OriginalContentUrl string
+    PreviewImageUrl string
+}
+type MessageContentAudio struct {
+    OriginalContentUrl string
+    Length int
 }
 
 type ErrorResponse struct {
@@ -75,7 +99,7 @@ func SendMessage(client http.Client, cred Credential, to []string, content Messa
     }
     req, err := http.NewRequest("POST", "https://trialbot-api.line.me/v1/events", bytes.NewBuffer(b))
     req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-    req.Header.Set("X-Line-ChannelID", cred.ChannelId)
+    req.Header.Set("X-Line-ChannelID", strconv.Itoa(cred.ChannelId))
     req.Header.Set("X-Line-ChannelSecret", cred.ChannelSecret)
     req.Header.Set("X-Line-Trusted-User-With-ACL", cred.ChannelMid)
 
@@ -94,6 +118,66 @@ func SendMessage(client http.Client, cred Credential, to []string, content Messa
     }
     defer res.Body.Close()
     return nil
+}
+
+func SendMessageText(client http.Client, cred Credential, to []string, text string) error {
+    return SendMessage(client, cred, to, MessageContent{
+        ContentType: ContentTypeText,
+        ToType: ToTypeUser,
+        Text: text,
+    })
+}
+
+func SendMessageImage(client http.Client, cred Credential, to []string, image MessageContentImage) error {
+    return SendMessage(client, cred, to, MessageContent{
+        ContentType: ContentTypeImage,
+        ToType: ToTypeUser,
+        OriginalContentUrl: image.OriginalContentUrl,
+        PreviewImageUrl: image.PreviewImageUrl,
+    })
+}
+
+func SendMessageVideo(client http.Client, cred Credential, to []string, video MessageContentVideo) error {
+    return SendMessage(client, cred, to, MessageContent{
+        ContentType: ContentTypeVideo,
+        ToType: ToTypeUser,
+        OriginalContentUrl: video.OriginalContentUrl,
+        PreviewImageUrl: video.PreviewImageUrl,
+    })
+}
+
+func SendMessageAudio(client http.Client, cred Credential, to []string, audio MessageContentAudio) error {
+    return SendMessage(client, cred, to, MessageContent{
+        ContentType: ContentTypeAudio,
+        ToType: ToTypeUser,
+        OriginalContentUrl: audio.OriginalContentUrl,
+        ContentMetadata: map[string]string{
+            "AUDLEN": strconv.Itoa(audio.Length),
+        },
+    })
+}
+
+func SendMessageLocation(client http.Client, cred Credential, to []string, location MessageContentLocation) error {
+    return SendMessage(client, cred, to, MessageContent{
+        ContentType: ContentTypeLocation,
+        ToType: ToTypeUser,
+        Location: &location,
+    })
+}
+
+func SendMessageSticker(client http.Client, cred Credential, to []string, sticker MessageContentSticker) error {
+    metadata := map[string]string{
+        "STKID": sticker.Id,
+        "STKPKGID": sticker.PackageId,
+    }
+    if (sticker.Version != "") {
+        metadata["STKVER"] = sticker.Version
+    }
+    return SendMessage(client, cred, to, MessageContent{
+        ContentType: ContentTypeSticker,
+        ToType: ToTypeUser,
+        ContentMetadata: metadata,
+    })
 }
 
 func ParseRequest(body io.Reader) (Result, error) {
